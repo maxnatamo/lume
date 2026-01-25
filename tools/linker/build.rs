f7861602943b68163d85cdd0c8e47d0ff60a7d9a
Max T. Kristiansen <me@maxtrier.dk>
2026-01-25 10:12:53 +0100

    test(linker): add C test files

---
+++ tools/linker/build.rs
@@ -0,0 +1,27 @@
+#![allow(clippy::disallowed_macros)]
+fn main() {
+    let cwd = std::env::current_dir().expect("current directory");
+    let testfiles = cwd.join("testfiles");
+
+    for file in testfiles.read_dir().unwrap() {
+        let source_file = file.unwrap();
+        let source_file_path = source_file.path();
+
+        let is_c_file = source_file_path
+            .extension()
+            .is_some_and(|ext| ext.to_str().unwrap() == "c");
+
+        if !source_file.file_type().unwrap().is_file() || !is_c_file {
+            continue;
+        }
+
+        println!("cargo::rerun-if-changed={}", source_file_path.display());
+
+        let mut object_file_path = source_file_path.clone();
+        object_file_path.set_extension("o");
+
+        for object_path in cc::Build::new().file(source_file_path).compile_intermediates() {
+            std::fs::copy(object_path, &object_file_path).unwrap();
+        }
+    }
+}
