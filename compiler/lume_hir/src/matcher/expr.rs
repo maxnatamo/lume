94c2bc5162dc2675088c4cd713935fe2d091d38a
Max T. Kristiansen <me@maxtrier.dk>
2026-08-01 11:52:44 +0200

    feat(hir/match): add matchers for all HIR expression types

--- compiler/lume_hir/src/matcher/expr.rs
+++ compiler/lume_hir/src/matcher/expr.rs
@@ -16,6 +16,188 @@ lumec_matchers! {
         }
     }
 
+    /// Matches all assignment expressions (`as`).
+    matcher assignment(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = Assignment;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::Assignment(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all cast expressions (`as`).
+    matcher cast(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = Cast;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::Cast(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all construct expressions.
+    matcher construct(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = Construct;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::Construct(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all dereference expressions (`*`).
+    matcher deref_expr(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = DerefExpr;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::Deref(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all reference expressions (`&`).
+    matcher ref_expr(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = RefExpr;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::Ref(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all intrinsic call expressions.
+    matcher intrinsic_call(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = IntrinsicCall;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::IntrinsicCall(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all instance call expressions.
+    matcher instance_call(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = InstanceCall;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::InstanceCall(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all static call expressions.
+    matcher static_call(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = StaticCall;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::StaticCall(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all `if` expressions.
+    matcher if_expr(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = If;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::If(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all `is` expressions.
+    matcher is(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = Is;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::Is(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all literal expressions.
+    matcher literal(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = Literal;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::Literal(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all member expressions.
+    matcher member(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = Member;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::Member(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
+    /// Matches all scope expressions.
+    matcher scope(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = Scope;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::Scope(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+
     /// Matches all variable references.
     matcher var_reference(predicates) -> bool {
         type Parent = Expression;
@@ -30,6 +212,22 @@ lumec_matchers! {
         }
     }
 
+    /// Matches all variant expressions.
+    matcher variant(predicates) -> bool {
+        type Parent = Expression;
+        type Subject = Variant;
+
+        fn satisfies(&self, hir: &Map, subject: &Self::Subject, bindings: &mut Bindings) -> bool {
+            if let ExpressionKind::Variant(subject) = &subject.kind {
+                Predicate::satisfies(&self.predicates, hir, subject, bindings)
+            } else {
+                false
+            }
+        }
+    }
+}
+
+lumec_matchers! {
     /// Matches all switch expressions.
     matcher switch(predicates) -> bool {
         type Parent = Expression;
